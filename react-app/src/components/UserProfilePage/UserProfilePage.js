c6632e91bcb34c1655155d367b85edec76fecacc
Le Nguyen <ltnguyen517@gmail.com>
2023-03-16 19:52:30 -0400

    still working on userprof pg

--- react-app/src/components/UserProfilePage/UserProfilePage.js
+++ react-app/src/components/UserProfilePage/UserProfilePage.js
@@ -10,7 +10,58 @@ import UserFollowing from "./UserFollowing";
 import "./UserProfilePage.css"
 
 const UserProfilePage = () => {
+    const history = useHistory()
+    const dispatch = useDispatch()
+    const sessionUser = useSelector((state) => state.session.user)
+    const playlistState = useSelector((state) => state.playlist)
+    const userFollowState = useSelector((state) => state.follows.people_user_follows)
 
+    const [user, setUser] = useState([])
+    const [everyUser, setEveryUser] = useState([])
+    const [sessionUserFollowers, setSessionUserFollowers] = useState([])
+    const [profileFollowers, setProfileFollowers] = useState([])
+    const [sessionUserFollowing, setSessionUserFollowing] = useState([])
+    const [followingPlaylists, setFollowingPlaylists] = useState([])
+    const [edit, setEdit] = useState(true)
+    let { userId } = useParams()
+
+    const userFollowing = []
+
+    useEffect(() => {
+        (async () => {
+            const userResponse = await fetch(`/api/users/${userId}`)
+            const user = await userResponse.json()
+            setUser(user)
+        })();
+        (async () => {
+            const everyUserResponse = await fetch("/api/users/")
+            const everyUser = await everyUserResponse.json()
+            setEveryUser(everyUser)
+        })();
+        (async () => {
+            if (sessionUser) {
+                const userFollowersArr = []
+                await dispatch(followingUserStore.getAllUserFollowers(sessionUser.id))
+            }
+        })();
+        (async () => {
+            const followInfo = await dispatch(followingUserStore.getAllUserFollowers(userId))
+            setProfileFollowers(followInfo)
+        })();
+        (async () => {
+            const playlistFollowResponse = await fetch(`/api/users/${userId}/playlists-followed`)
+            const playlistFollowInfo = await playlistFollowResponse.json()
+            setFollowingPlaylists(playlistFollowInfo.following)
+        })();
+        (async () => {
+            if(sessionUser){
+                const data = await dispatch(followingUserStore.getAllUserFollowers(sessionUser?.id))
+                setSessionUserFollowing(data)
+            }
+        })();
+    }, [userId, sessionUser?.id, setSessionUserFollowing, setUser, dispatch, setSessionUserFollowers, setProfileFollowers, edit, setFollowingPlaylists])
+
+    profileFollowers?.
 };
 
 export default UserProfilePage;
