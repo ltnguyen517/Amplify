7c220157105f882f69552a0703f739e03135811b
Le Nguyen <ltnguyen517@gmail.com>
2023-03-17 16:30:42 -0400

    working on user prof page

--- react-app/src/components/UserProfilePage/UserProfilePage.js
+++ react-app/src/components/UserProfilePage/UserProfilePage.js
@@ -61,7 +61,87 @@ const UserProfilePage = () => {
         })();
     }, [userId, sessionUser?.id, setSessionUserFollowing, setUser, dispatch, setSessionUserFollowers, setProfileFollowers, edit, setFollowingPlaylists])
 
-    profileFollowers?.
+    profileFollowers?.people_user_follows?.map(async (id) => {
+        const response = await fetch(`/api/users/${id}`)
+        const data = await response.json()
+        userFollowing.push(data)
+    })
+
+    if(userId > everyUser?.users?.length) return <Error404Page />
+
+    let userFollowingArr = []
+    let userFollowerArr = []
+
+    for (let i = 0; i < everyUser?.users?.length; i++){
+        let userId = everyUser.users[i].id
+        for (let j = 0; j < profileFollowers?.people_user_follows?.length; j++){
+            let anotherUserId = profileFollowers.people_user_follows[j]
+            if (userId === anotherUserId){
+                userFollowingArr.push(everyUser.users[i])
+            }
+        }
+    }
+
+    for (let i = 0; i < everyUser?.users?.length; i++){
+        let userId = everyUser.users[i].id
+        for (let j = 0; j < profileFollowers?.followers_of_user?.length; j++){
+            let anotherUserId = profileFollowers.followers_of_user[j]
+            if(userId === anotherUserId){
+                userFollowerArr.push(everyUser.users[i])
+            }
+        }
+    }
+
+    let followerProfileImg
+
+    if(!sessionUserFollowers) return null
+
+    if(!user.profile_picture){
+        followerProfileImg = <i class="fa-solid fa-user fa-4x"></i>
+    } else {
+        followerProfileImg = <img src={sessionUser.profile_picture}/>
+    }
+
+    const playlistArray = Object.values(playlistState)
+    const userPlaylistsList = playlistArray.filter(playlist => Number(playlist?.User?.id) === Number(userId))
+
+    let lengthUserPlaylists
+    if(userPlaylistsList.length === 1){
+        lengthUserPlaylists = <span>{userPlaylistsList.length} public playlist</span>
+    } else {
+        lengthUserPlaylists = <span>{userPlaylistsList.length} public playlists</span>
+    }
+
+    let toFollowButton
+    if(sessionUser !== null){
+        if(userFollowState.includes(user.id)){
+            toFollowButton = (<button className="unfollowbutton" hidden={sessionUser.id === user.id} onClick={(e) => { unFollow(e); setEdit(!edit) }}>Unfollow</button>)
+        } else {
+            toFollowButton = (<button className="followbutton" hidden={sessionUser.id === user.id} onClick={(e) => { follow(e); setEdit(!edit) }}>Follow</button>)
+        }
+    } else {
+        toFollowButton = (<button className="followbutton" onClick={() => history.push("/login")}>Follow</button>)
+    }
+
+    const unFollow = (e) => {
+        setEdit(true)
+        e.preventDefault()
+        dispatch(followingUserStore.unfollowAUser(sessionUser.id, userId))
+    }
+
+    const follow = (e) => {
+        setEdit(true)
+        e.preventDefault()
+        dispatch(followingUserStore.followAUser(sessionUser.id, userId))
+    }
+
+    if(sessionUser){
+        if(sessionUser.id === Number(userId)){
+            return (
+                
+            )
+        }
+    }
 };
 
 export default UserProfilePage;
