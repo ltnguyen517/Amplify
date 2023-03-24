eab7307b586753e07b4e10f290424e274ce41cfe
Le Nguyen <ltnguyen517@gmail.com>
2023-03-24 13:08:07 -0400

    seed fixing

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,25 +20,28 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_artists()
+
         undo_songs()
         undo_albums()
+        undo_artists()
         # undo_playlists()
         undo_users()
     seed_users()
     # seed_playlists()
+    seed_artists()
     seed_albums()
     seed_songs()
-    seed_artists()
+
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_artists()
+
     undo_songs()
     undo_albums()
+    undo_artists()
     # undo_playlists()
     undo_users()
     # Add other undo functions here
